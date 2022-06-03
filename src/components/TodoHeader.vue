ae8aaf383a984df16d61659085818161572ffa9f
eun6304 <a01050412736@gmail.com>
2022-06-03 20:02:10 +0900

    마이페이지 만들거야

--- src/components/TodoHeader.vue
+++ src/components/TodoHeader.vue
@@ -1,5 +1,6 @@
 <template>
-  <header>
+  <header>    
+    <i class="myPage fa-solid fa-alien"></i>
     <h1>TODO it!</h1>
   </header>
 </template>
@@ -15,5 +16,11 @@ export default {
     color : #2f3b52;
     font-weight : 900;
     margin : 2.5rem 0 1.5rem;
+  }
+
+  .myPage {
+    line-height: 45px;
+    color: #62acde;
+    margin-right: 5px;
   }
 </style>
